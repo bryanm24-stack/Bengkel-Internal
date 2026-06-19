0456638d3b48a00823b406ee64d8279288b02a61
bry-m <bryan.m24@mhs.istts.ac.id>
2026-06-19 09:46:33 +0700

    Main

--- src/preload/index.js
+++ src/preload/index.js
@@ -3,11 +3,22 @@ import { electronAPI } from '@electron-toolkit/preload'
 
 // Custom APIs for renderer and maintenance system APIs
 const api = {
- 
+  printPDF: () => ipcRenderer.invoke('printPDF'),
+  completeRepair: (payload) => ipcRenderer.invoke('completeRepair', payload),
+  getKendaraan: () => ipcRenderer.invoke('getKendaraan'),
   getCategories: () => ipcRenderer.invoke('getCategories'),
   getSukuByKategori: (kategori) => ipcRenderer.invoke('getSukuByKategori', kategori),
+  getReports: () => ipcRenderer.invoke('getReports'),
   login: (username, password) => ipcRenderer.invoke('login', username, password),
   addSukuCadang: (payload) => ipcRenderer.invoke('addSukuCadang', payload),
+  addKendaraan: (payload) => ipcRenderer.invoke('addKendaraan', payload),
+  updateKendaraanStatus: (payload) => ipcRenderer.invoke('updateKendaraanStatus', payload),
+  getMechanics: () => ipcRenderer.invoke('getMechanics'),
+  assignRepair: (payload) => ipcRenderer.invoke('assignRepair', payload),
+  getKendaraanForMekanik: (id_user) => ipcRenderer.invoke('getKendaraanForMekanik', id_user),
+  getAssignedRepairByVehicle: (payload) => ipcRenderer.invoke('getAssignedRepairByVehicle', payload),
+  addLogPerbaikan: (payload) => ipcRenderer.invoke('addLogPerbaikan', payload),
+  deleteKendaraan: (nomor_polisi) => ipcRenderer.invoke('deleteKendaraan', nomor_polisi)
 }
 
 // Use `contextBridge` APIs to expose Electron APIs to
